833d7c971b11fe3e0d5086cd94e82d73df35b75a
Terrence Meikle <terrence_meikle@yahoo.com>
2022-04-23 17:29:50 -0400

    work on second page HTML and JS

--- assets/recipe.js
+++ assets/recipe.js
@@ -1,3 +1,6 @@
+const topSection = document.getElementById('top-section');
+const ingredients = document.getElementById('ingredients');
+const nutritionalFacts = document.getElementById('nutritional-facts');
 let currentSearch;
 let currentRecipe = JSON.parse(localStorage.getItem('clickedRecipe'));
 
@@ -15,9 +18,42 @@ let test1 = currentSearch.hits.filter(function (item, index) {
   }
 });
 
-// let test1 = currentSearch.hits.map((item, index) => {
-//   if (item.recipe.label === currentRecipe) {
-//     return;
-//   }
-// });
 console.log(test1);
+topSection.innerHTML = test1.map((item, index) => {
+  return `<div>
+  <p class = "fs-3 fw-bold">${item.recipe.label}</p>
+  </div>
+  <div class="m-1">
+  <img src=${item.recipe.images.REGULAR.url}
+  class="card-img-top  p-2" alt="">
+  </div>`;
+});
+
+let ingredientsList = test1[0].recipe.ingredientLines;
+
+ingredients.innerHTML = ingredientsList
+  .map((item) => {
+    return `<li>${item}</li>`;
+  })
+  .join(' ');
+
+let nutritionalVal = test1[0].recipe.totalNutrients;
+console.log(nutritionalVal);
+
+// for (let i=0; i<nutritionalVal.length; i++) {
+
+// }
+for (let val in nutritionalVal) {
+  let testVal = val;
+  console.log(val);
+  console.log(nutritionalVal[val].label);
+  let newVal = [];
+
+  newVal.push(testVal);
+  console.log(newVal);
+  // nutritionalFacts.innerHTML = `<li>${nutritionalVal[val].label} ${nutritionalVal[val].unit}</li>`;
+}
+
+// nutritionalFacts.innerHTML = nutritionalVal.map((item) => {
+//   return `<li>${item.label}${item.unit}</li>`;
+// });
